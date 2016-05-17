4a7850c342648c1bdde96f9759c0d6a6599dcec5
Ryan Bard <john.ryan.bard@gmail.com>
2016-05-16 22:43:04 -0400

    Added firefox 45 esr to my browser aliases

--- browsers/browsers_env.bash
+++ browsers/browsers_env.bash
@@ -7,10 +7,12 @@ then
     alias ff24='~/tools/firefox/ff24esr/Firefox.app/Contents/MacOS/parameterized-app.sh'
     alias ff31='~/tools/firefox/ff31esr/Firefox.app/Contents/MacOS/parameterized-app.sh'
     alias ff38='~/tools/firefox/ff38esr/Firefox.app/Contents/MacOS/parameterized-app.sh'
+    alias ff45='~/tools/firefox/ff45esr/Firefox.app/Contents/MacOS/parameterized-app.sh'
 else
     alias ff10='~/tools/firefox/ff10esr/firefox/firefox -P ff10 -no-remote'
     alias ff17='~/tools/firefox/ff17esr/firefox/firefox -P ff17 -no-remote'
     alias ff24='~/tools/firefox/ff24esr/firefox/firefox -P ff24 -no-remote'
     alias ff31='~/tools/firefox/ff31esr/firefox/firefox -P ff31 -no-remote'
     alias ff38='~/tools/firefox/ff38esr/firefox/firefox -P ff38 -no-remote'
+    alias ff45='~/tools/firefox/ff45esr/firefox/firefox -P ff45 -no-remote'
 fi
